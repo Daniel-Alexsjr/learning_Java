b1c12fd821496b163ca8b4d0df2304b3268a51b4
Daniel Alexandre <danielalexandresjr@gmail.com>
2025-06-17 11:20:03 -0300

    Update sortAndStar.java

--- fundamental kata/sortAndStar.java
+++ fundamental kata/sortAndStar.java
@@ -5,15 +5,28 @@
 
 public class sortAndStar {
 
-    public static String twoSort(String[] s) {
+    
+
+    public static String addStarFirstStr(String[] s) {
         java.util.Arrays.sort(s);
 
         return String.join("***",s[0].split(""));
     }
 
+    public static String addStartsAll(String s){
+        return String.join("***", s.split(""));
+    }
+
     public static void main(String[] args) {
+        // Example usage:
+        // Sort the names and return the first one with "***" between each letter
         String[] names = {"John", "Maria", "José", "Ana", "Pedro"};
-        System.out.println(twoSort(names));
+        System.out.println(addStarFirstStr(names));
+
+        // Print all names with "***" between each letter
+        for (String name : names){
+            System.out.println(addStartsAll(name));
+        }
     }
 
 }
